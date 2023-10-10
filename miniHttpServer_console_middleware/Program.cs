68bef898646baeb142213a576cc593526ccf66f3
Vusal Aliyev <vusal.aliyev7472@gmail.com>
2023-10-10 16:47:00 +0400

    initial commit

---
+++ miniHttpServer_console_middleware/Program.cs
@@ -0,0 +1,68 @@
+﻿
+
+using System.Net;
+
+
+new Webhost(27001).Run();
+
+class Webhost
+{
+    int port;
+    string pathBase = @"C:/Users/user/Desktop/Step dersler/Nadir html.css/Lesson 7/code/";
+    HttpListener listener;
+
+    public Webhost(int port)
+    {
+        this.port = port;
+    }
+
+    public void Run()
+    {
+        listener = new HttpListener();
+        listener.Prefixes.Add($@"http://localhost:{port}/");
+        listener.Start();
+        Console.WriteLine($"Http server started on {port}");
+        while (true)
+        {
+            var context = listener.GetContext();
+            Task.Run(() =>
+            {
+                HandleRequest(context);
+            });
+        }
+
+
+    }
+
+
+    private void HandleRequest(HttpListenerContext context)
+    {
+        
+        var url = context.Request.RawUrl;
+        var path = $@"{pathBase}{url.Split('/').Last()}";
+
+        var responce = context.Response;
+
+        StreamWriter streamWriter = new StreamWriter(responce.OutputStream);
+
+       try
+       {
+
+            var src = File.ReadAllText(path);
+            streamWriter.Write(src);
+       }
+       catch(Exception ex)
+       {
+            //Console.WriteLine(ex.Message);
+            var src = $@"{pathBase}404.html";
+            streamWriter.Write(src);
+       }
+       finally
+       {
+            streamWriter.Close();
+       }
+
+    }
+
+
+}
